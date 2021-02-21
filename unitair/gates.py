47ab7f3bc1f05498b40456fe58522d92cd180a0b
sjasonw <sjasonw@gmail.com>
2021-02-21 09:10:57 -0800

    Introduced constant gates as opposed to parameterized gates.

--- unitair/gates.py
+++ unitair/gates.py
@@ -1,10 +1,12 @@
 import torch
-from typing import Union
+from typing import Union, Optional
 import decorator
+from unitair import get_default_device
+from unitair import Field
 
 
 @decorator.decorator
-def single_qubit_gate(gate_function, *args):
+def parameterized_gate(gate_function, *args):
     """Build a single qubit gate given a gate function.
 
     This is intended to be used as a decorator. The gate function takes
@@ -32,7 +34,28 @@ def single_qubit_gate(gate_function, *args):
     return gate
 
 
-@single_qubit_gate
+@decorator.decorator
+def constant_gate(gate_function, real_or_imag: Optional[str] = None, *args):
+    device = args[0]
+    if device is None:
+        device = get_default_device()
+    gate = torch.tensor(gate_function(), device=device)
+    if real_or_imag is None:
+        return gate
+    else:
+        zeros = torch.zeros(size=gate.size(), device=device)
+        if real_or_imag.lower() == 'real':
+            complex_gate = [gate, zeros]
+        elif real_or_imag.lower() == 'imag':
+            complex_gate = [zeros, gate]
+        else:
+            raise ValueError(
+                'argument real_or_imag should be \'real\' or \'imag\'.\n'
+                f'Found {real_or_imag}.')
+        return torch.stack(complex_gate)
+
+
+@parameterized_gate
 def exp_x(angle: Union[torch.Tensor, float]):
     """Get the operator e^(-i angle X).
 
@@ -56,9 +79,18 @@ def exp_x(angle: Union[torch.Tensor, float]):
     ]
 
 
-@single_qubit_gate
+@parameterized_gate
 def exp_y(angle: Union[torch.Tensor, float]):
-    """Get the operator e^(-i angle Y)."""
+    """Get the operator e^(-i angle Y).
+
+    Args:
+        angle: Tensor with size (batch_length,) or just ().
+
+    Returns: Tensor with size (batch_length, 2, 2, 2) or (2, 2, 2) if there
+        is no batch dimension. The (2, 2, 2) is such that the first dimension
+        means the real and imaginary parts and the last two dimension are
+        the matrices of the real an imaginary parts of the gates.
+    """
     cos = torch.cos(angle)
     sin = torch.sin(angle)
     zero = torch.zeros(angle.size(), device=angle.device)
@@ -71,9 +103,18 @@ def exp_y(angle: Union[torch.Tensor, float]):
     ]
 
 
-@single_qubit_gate
+@parameterized_gate
 def exp_z(angle: Union[torch.Tensor, float]):
-    """Get the operator e^(-i angle Z)."""
+    """Get the operator e^(-i angle Z).
+
+    Args:
+        angle: Tensor with size (batch_length,) or just ().
+
+    Returns: Tensor with size (batch_length, 2, 2, 2) or (2, 2, 2) if there
+        is no batch dimension. The (2, 2, 2) is such that the first dimension
+        means the real and imaginary parts and the last two dimension are
+        the matrices of the real an imaginary parts of the gates.
+    """
     cos = torch.cos(angle)
     sin = torch.sin(angle)
     zero = torch.zeros(angle.size(), device=angle.device)
@@ -84,6 +125,38 @@ def exp_z(angle: Union[torch.Tensor, float]):
         [[-sin, zero],
          [zero, sin]]
     ]
+
+@constant_gate(real_or_imag='real')
+def pauli_x(device: Optional[torch.device] = None):
+    return [
+        [0., 1.],
+        [1., 0.]
+    ]
+
+@constant_gate(real_or_imag='imag')
+def pauli_y(device: Optional[torch.device] = None):
+    return [
+        [0., -1.],
+        [1., 0.]
+    ]
+
+@constant_gate(real_or_imag='real')
+def pauli_z(device: Optional[torch.device] = None):
+    return [
+        [1., 0.],
+        [0., -1.]
+    ]
+
+
+@constant_gate(real_or_imag='real')
+def cnot(device: Optional[torch.device] = None):
+    return [
+        [1., 0., 0., 0.],
+        [0., 1., 0., 0.],
+        [0., 0., 0., 1.],
+        [0., 0., 1., 0.]
+    ]
+
 
 
 def nested_stack(params, roll: bool = False):

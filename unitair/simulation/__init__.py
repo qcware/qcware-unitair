258baa2f3739c4cf0b64726261977c83589d47ed
sjasonw <sjasonw@gmail.com>
2021-02-25 17:02:46 -0800

    added measure to simulation/__init__

--- unitair/simulation/__init__.py
+++ unitair/simulation/__init__.py
@@ -5,3 +5,4 @@ from .operations import act_first_qubit
 from .operations import act_last_qubit
 from .operations import swap
 from .operations import act_first_qubits
+from .measurement import measure

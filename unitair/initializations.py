acef0d49c2fe17312e02208bda30036e73119256
sjasonw <sjasonw@gmail.com>
2021-07-05 10:07:50 -0700

    Allow batch dimensions for initializations.
    Introduce batch_dims parameter for rand_state and uniform
    superposition in initializations.py.

--- unitair/initializations.py
+++ unitair/initializations.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Sequence, Optional
 
 import torch
 
@@ -28,13 +28,17 @@ def unit_vector(
     return vector
 
 
-# TODO: add batch dimensions!
 def rand_state(
         num_qubits,
+        batch_dims: Optional[Sequence] = None,
         device: torch.device = None,
         field: Union[Field, str] = Field.COMPLEX,
         requires_grad: bool = False
 ) -> torch.Tensor:
+    if batch_dims is None:
+        batch_dims = torch.Size()
+    else:
+        batch_dims = torch.Size(batch_dims)
     field = Field(field.lower())
     if device is None:
         device = unitair.get_default_device()
@@ -45,20 +49,39 @@ def rand_state(
         size = (2 ** num_qubits,)
     else:
         assert False, f"Impossible enumeration {field}."
+    state_num_dims = len(size)
 
+    size = batch_dims + size
     state = torch.rand(
         size,
-        device=device, requires_grad=requires_grad
+        device=device,
+        requires_grad=requires_grad
     )
-
     norm = states.norm_squared(state, field=field).sqrt()
-    return state / norm
+    norm_size_for_division = norm.size() + (1,) * state_num_dims
+    return state / norm.view(norm_size_for_division)
 
 
-def uniform_superposition(num_qubits: int, device: torch.device = None):
-    """Create the uniform superposition state |+...+>."""
+def uniform_superposition(
+        num_qubits: int,
+        batch_dims: Optional[Sequence] = None,
+        device: torch.device = None,
+        requires_grad: bool = False
+):
+    """Create the uniform superposition state |+...+>.
+
+    This function is defined for the complex case only. Optional batch
+    dimensions can be provided in which case the same uniform superposition
+    is copied to form a batch with specified shape.
+    """
     if device is None:
         device = unitair.get_default_device()
     real = 2**(-num_qubits/2.) * torch.ones(2**num_qubits, device=device)
     imag = torch.zeros(2**num_qubits, device=device)
-    return torch.stack((real, imag), dim=0)
+    state = torch.stack((real, imag), dim=0)
+    if batch_dims is not None:
+        batch_dims = torch.Size(batch_dims)
+        state = state.repeat(batch_dims + (1, 1))
+
+    state.requires_grad_(requires_grad)
+    return state

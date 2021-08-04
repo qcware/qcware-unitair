ff41754692bf8143b3078de68886348b3dffa84a
sjasonw <sjasonw@gmail.com>
2021-08-04 13:01:08 -0700

    Remove old ref to get_default_device.

--- unitair/utils.py
+++ unitair/utils.py
@@ -3,11 +3,14 @@ from typing import Optional
 import unitair
 
 
-def int_to_bin(x: int, num_bits: int, str_output: bool = False, device: Optional[torch.device] = None):
+def int_to_bin(
+        x: int,
+        num_bits: int,
+        str_output: bool = False,
+        device: torch.device = torch.device("cpu")
+):
     if x >= 2 ** num_bits:
         raise ValueError(f"Insufficient bits to store integer {x}.")
-    if device is None:
-        device = unitair.get_default_device()
 
     bin_format = [int(d) for d in str(bin(x))[2:]]
     padding_size = num_bits - len(bin_format)

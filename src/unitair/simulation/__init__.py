a72d6ae9061c24b4f30df4dff5b3a725bb8afd1e
sjasonw <sjasonw@gmail.com>
2022-03-16 14:08:59 -0700

    Switch to setup.cfg, move unitair into src

--- src/unitair/simulation/__init__.py
+++ src/unitair/simulation/__init__.py
@@ -4,6 +4,8 @@ from .operations import apply_to_qubits
 from .operations import apply_all_qubits
 from .operations import act_last_qubit
 from .operations import swap
+from .operations import multi_cz
+from .operations import multi_controlled_x, multi_controlled_z
 from .operations import act_first_qubits
 from .measurement import measure
 
